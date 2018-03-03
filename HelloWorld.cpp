7223d149ed2ab523cb151abc0f6d32394c3e72b1
yatal0w <yatal0w@gmail.com>
2018-03-04 00:42:49 +0900

    divide the cpp source

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,30 +1,13 @@
-#include <stdio.h>
+#include "HelloWorld.h"
 
-class Sample
+Sample::Sample()
 {
-public:
-    Sample();
-    ~Sample();
-    int minus(int a, int b){ return a-b; }
-};
-
-Sample::Sample(){}
-Sample::~Sample(){}
-int plus(int a, int b);
-int main()
+}
+Sample::~Sample()
 {
-    printf("HelloWorld\n");
-    int a=10,b=20;
-    int c=plus(a,b);
-    printf("plus=%d\n",c);
-    Sample* sample = new Sample();
-    int d=sample->minus(a,b);
-    printf("minus=%d\n",d);
-    delete sample;
-    return 0;
+}
+int Sample::minus(int a, int b)
+{
+    return a-b;
 }
 
-int plus(int a, int b)
-{
-    return a+b;
-}
